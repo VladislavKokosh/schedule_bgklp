4a1805a5411b0ef35675aed4a6616e4e5e0825eb
iamkoks <iamkoksss@mail.ru>
2021-07-16 01:04:09 +0300

    App and index to tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 
 import App from './App';
-import reducer  from './store/reducers/reducer.ts';
-import rootSaga from './store/sagas/index.ts';
+import reducer  from './store/reducers/reducer';
+import rootSaga from './store/sagas/index';
 
 const saga = createSagaMiddleware();
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(saga)))
@@ -21,8 +20,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
