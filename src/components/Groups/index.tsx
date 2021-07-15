232899ecc50fa8ae69bee989a5abb4a2315c6ca6
iamkoks <iamkoksss@mail.ru>
2021-07-15 23:56:16 +0300

    Groups to tsx

--- src/components/Groups/index.tsx
+++ src/components/Groups/index.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, FC, ChangeEvent } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { getGroupsAsync, setGroupsAsyncSuccess } from '../../store/actions/groups';
 import { getGroupsSelector } from '../../store/selectors/groups';
 import './index.scss'
 
-const Groups = () => {
+const Groups:FC = () => {
   const dispatch = useDispatch();
   const groups = useSelector(getGroupsSelector)
   useEffect(()=> {
@@ -15,7 +15,7 @@ const Groups = () => {
 
   let date = new Date();
 
-  const selectGroup = (e) => {
+  const selectGroup = (e:ChangeEvent<HTMLSelectElement>) => {
     dispatch(setGroupsAsyncSuccess(e.target.value))
   }
 
@@ -24,8 +24,7 @@ const Groups = () => {
       <h2>Расписание занятий для студентов БГКЛП</h2>
       <h4>{date.getMonth() > 6 ? 'первый семестр, '+(date.getFullYear()-1)+'-'+date.getFullYear()+' учебный год': 'второй семестр, '+(date.getFullYear()-1)+'-'+date.getFullYear()+' учебный год'}</h4>
       <div className='select'>
-        <select onChange={(e) => selectGroup(e)} disabled={groups ? false  : true}>
-          <option autoFocus>Выберите группу...</option>
+        <select onChange={(e) => selectGroup(e)} disabled={!groups.length} placeholder='Выберите группу...'>
           {groups?.map((group) => <option key={group.id} value={group.title}>{group.title}</option>)}
         </select>
       </div>
