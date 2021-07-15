9a09516f86f033ad4237e476ec246635e98471d1
iamkoks <iamkoksss@mail.ru>
2021-07-16 01:00:04 +0300

    all pages to tsx

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { FC } from 'react'
 
 import Contacts from '../components/Contacts'
 
-const ContactPage = () => {
+const ContactPage: FC = () => {
   return (
     <>
       <Contacts/>
--- src/pages/TeachersPage.tsx
+++ src/pages/TeachersPage.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { FC } from 'react'
 
 import Teachers from '../components/Teachers'
 
-const TeachersPage = () => {
+const TeachersPage: FC = () => {
   return (
     <>
       <Teachers/>
